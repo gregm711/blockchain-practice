719c149b7ad53b0f6d31846178d69b8cd64b3b34
gregm711 <gregm@wearevest.com>
2017-12-28 11:50:46 -0500

    coming along

---
+++ tutorial/pow/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+// IntToHex converts an int64 to a byte array
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
+
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to Greg")
+	bc.AddBlock("Send 2 more BTC to Greg")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+}
